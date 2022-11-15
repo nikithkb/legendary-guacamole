689646a59aee61d8deca4b33e3b79271a2c35434
poorvisuparn <poorvisuparn@gmail.com>
2022-11-15 11:11:42 +0530

    algorithm change

--- algorithm.py
+++ algorithm.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 # %%
 # INTEGRATE
-data = pd.read_csv('C:\Poorvi\Code\VScode\Sem5\SEProject\Book1.csv')
-
+data = pd.read_csv('userData.csv')
+data.drop('Timestamp', axis=1, inplace = True)
 data.head(7)
 
 # %%
@@ -20,7 +20,7 @@ data.head(7)
 df['Score'] = ""
 def calc_score (username):
     for i in range(len(data)):
-        score = 1 * data.loc[i, "MVP"] + 0.9 * data.loc[i, "Wins"] + 0.8 * data.loc[i, "Goals"] + 0.6 * data.loc[i, "Assist"] + 0.4 * data.loc[i, "Saves"] + 0.3 * data.loc[i, "Shots"]
+        score = 1 * data.loc[i, "MVPs"] + 0.9 * data.loc[i, "Wins"] + 0.8 * data.loc[i, "Goals"] + 0.6 * data.loc[i, "Assists"] + 0.4 * data.loc[i, "Saves"] + 0.3 * data.loc[i, "Shots"]
         # max score = 4
         df.loc[i, 'Score'] = score 
     return round(score, 4)
@@ -43,22 +43,16 @@ username = 'tempy'
 # %%
 # INTEGRATE
 def find_team(username):
-    team = []
     idx = df.index.get_loc(username)
     if idx == 0:
-        team.append(df.iloc[idx : idx + 3])
+        team = (df.iloc[idx : idx + 3]).copy()
     elif idx == len(df)-1:
-        team.append(df.iloc[idx - 2 : idx + 1])
+        team = (df.iloc[idx - 2 : idx + 1]).copy()
     else:
-        team.append(df.iloc[idx - 1 : idx + 2])
+        team = (df.iloc[idx - 1 : idx + 2]).copy()
     return team
 
-print(find_team(username))
+find_team(username).to_csv('Team.csv')
 
-
-# %%
-from sqlalchemy import create_engine
-engine = create_engine('sqlite:///my.db', echo=False)
-data.to_sql('my_team', con=engine, if_exists='append')
 
 
